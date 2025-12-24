3d09f3f3f5aa796a2712744c7c3d23513acdbecc
rafsan102 <ahmedrafsan101@gmail.com>
2025-12-25 03:26:12 +0600

    [added] added product section

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Banner from "./components/Banner";
 import CategorySection from "./components/CategorySection";
+import ProductSection from "./components/ProductSection";
 
 export default function Home() {
   return (
     <main className="mt-10">
       <Banner />
       <CategorySection />
+      <ProductSection />
     </main>
   );
 }
